2fe3e287e85e1dea472517f8c211e4cefe66204a
MikePio <michelepiopilla@gmail.com>
2023-06-12 19:34:51 +0200

    Create the table columns

--- database/migrations/2023_06_12_161710_create_d_c_comics_table.php
+++ database/migrations/2023_06_12_161710_create_d_c_comics_table.php
@@ -15,7 +15,20 @@ return new class extends Migration
     {
         Schema::create('d_c_comics', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('thumb');
+            $table->string('price', 8);
+            $table->string('series', 50);
+            $table->string('sale_date', 10);
+            $table->string('type', 50);
+            $table->string('artists');
+            $table->string('writers');
+
             $table->timestamps();
+
+            //* DOPO AVER SCRITTO LE COOLONNE DELLA TABELLA INSERIRE IL COMANDO php artisan migrate
         });
     }
 
